07556f7c345e70f094d1267050a719433013a952
= <dadu3890@colorado.edu>
2020-11-19 12:36:19 -0700

    Server delivers static react files
    Also runs on port 5k and uses a post request for the search bar

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 import express from 'express';
 import axios from 'axios';
+import path from 'path';
+
+const REACT_BUNDLE_PATH = path.resolve('./') + '/app/build';
 
 const app = express();
+app.use(express.static(REACT_BUNDLE_PATH));
 app.use(express.json());
 
 //Cache the city data
 const dataEndpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
 const endpointResponse = await axios.get(dataEndpoint);
 const cityData = endpointResponse.data;
-console.log('- Retrieved city data');
 
 //Search for the given string in the JSON data
-app.get('/search', (req, res) => {
+app.post('/search', (req, res) => {
     try {
         const input = req.body['input'];
 
         if (!input) {
-            res.status(400).send('Request failed: improper formatting');
+            res.status(400).json([{ error: 'Request failed: improper formatting' }]);
         }
         else {
             const inputRegex = new RegExp(input.toLowerCase());
@@ -35,7 +37,12 @@ app.get('/search', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 2000;
+//Serve static React files
+app.get('/', (req, res) => {
+    res.sendFile(`${REACT_BUNDLE_PATH}/index.html`);
+});
+
+const PORT = process.env.PORT || 5000;
 const appServer = app.listen(PORT, () => {
     console.log('- Server listening on port', PORT);
 });
