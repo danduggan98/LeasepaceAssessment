8e7bc1a6c94de563db2bf2868afe2115af4ea5b2
= <dadu3890@colorado.edu>
2020-11-20 08:04:39 -0700

    Check for valid requests

--- server.js
+++ server.js
@@ -9,17 +9,23 @@ app.use(express.static(REACT_BUNDLE_PATH));
 app.use(express.json());
 
 //Cache the city data
-const dataEndpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const dataEndpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29' +
+                     '/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
 const endpointResponse = await axios.get(dataEndpoint);
 const cityData = endpointResponse.data;
+console.log('- Data retrieved');
 
 //Search for the given string in the JSON data
 app.post('/search', (req, res) => {
     try {
         const input = req.body['input'];
-
-        if (!input) {
-            res.status(400).json([{ error: 'Request failed: improper formatting' }]);
+        
+        if (typeof(input) === 'undefined') {
+            res.status(400).json({ error: 'Request failed: improper formatting' });
+        }
+        else if (!input.length) {
+            res.status(200).json([]);
         }
         else {
             const inputRegex = new RegExp(input.toLowerCase());
