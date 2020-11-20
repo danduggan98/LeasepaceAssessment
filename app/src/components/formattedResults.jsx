766e78e2aa86a6ad98bde2e976fd4db57a1e4d4d
= <dadu3890@colorado.edu>
2020-11-20 12:39:06 -0700

    Show all data for each city
    Also shows an error message if the search fails

--- app/src/components/formattedResults.jsx
+++ app/src/components/formattedResults.jsx
@@ -1,15 +1,25 @@
+import '../styles/formattedResults.css';
+
 const FormattedResults = (props) => {
     const cityList = props.data.map(
         city => (
             <div className='cityData'>
-                <div>{city.city}, {city.state}</div>
-                { /* Additional data here */ }
+                <div>{city['city']}, {city['state']}</div>
+                <div>Population: {city['population']}</div>
+                <div>Latitude: {city['latitude']}</div>
+                <div>Longitude: {city['longitude']}</div>
+                <div>Growth (2000-2013): {city['growth_from_2000_to_2013']}</div>
             </div>
         )
-    )
+    );
     
     return (
-        <div>{cityList}</div>
+        <div>
+            { cityList.length
+              ? cityList
+              : 'None - please try again'
+            }
+        </div>
     )
 }
 
